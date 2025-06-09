83e010fc4cd272dfc826a476cefb971a852c7080
ph4mished <jbotchwayowusu@gmail.com>
2025-06-09 12:17:03 +0000

    Update banner.go

--- core/utils/banner.go
+++ core/utils/banner.go
@@ -10,7 +10,5 @@ func Banner(){
 	fmt.Printf("%sDISCLAIMER:%s The Developer Is Not In Any Way Responsible Or Liable For Any%s\n", bred, bylw, rst)
 	fmt.Printf("%s                     Misuse Or Damage Caused With PassCrax%s\n", bylw, rst)
 	fmt.Printf("%s################################################################################%s\n\n",bcyn, rst) 
-	fmt.Printf("%sType '%shelp%s%s' to see available commands.%s\n", grn, bylw, rst, grn, rst) 
-	fmt.Printf("%sIf the content of '%shelp%s%s' got you confused, just type '%srun%s%s' to get started.%s\n", grn, bylw, rst, grn, bylw, rst, grn, rst)
-	
+	fmt.Printf("%sType '%shelp%s%s' to see available commands.%s\n", grn, bylw, rst, grn, rst)	
 }
